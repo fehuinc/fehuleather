84ee2ffac3269cdf9620d6cc2df12da46b1aba51
Ivan Reese <ivanreese@gmail.com>
2015-08-26 15:30:52 -0600

    Add per-product info in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ rings = Kingdom.create! name: "Ring"
 # Raw Ring ######################################
 raw_ring = rings.products.create! name: "Raw Ring", show_retail: true, show_wholesale: true, price_retail: 100, price_wholesale: 50, ypos: 23
 
-raw_ring.infos.create! name: "Tips", content: "Tips Content"
-raw_ring.infos.create! name: "Sizing", content: "Sizing Content"
-raw_ring.infos.create! name: "Gifting", content: "Gifting Content"
-raw_ring.infos.create! name: "Uniqueness", content: "Uniqueness Content"
+raw_ring.infos.create! name: "Tips", content: "Raw Ring Tips Content"
+raw_ring.infos.create! name: "Sizing", content: "Raw Ring Sizing Content"
+raw_ring.infos.create! name: "Gifting", content: "Raw Ring Gifting Content"
+raw_ring.infos.create! name: "Uniqueness", content: "Raw Ring Uniqueness Content"
 
 raw_ring_leather_color = raw_ring.variations.create! name: "Leather Color", level: 0, has_image: true
 raw_ring_leather_color.variants.create! name: "Red", show_retail: true, show_wholesale: true, description: "A fetching slice of saddle leather that starts off richly red and ages to a wealthy coffee brown."
@@ -25,10 +25,10 @@ bracelets = Kingdom.create! name: "Bracelet"
 # C&C Bracelet ##################################
 cc_bracelet = bracelets.products.create! name: "Coffee & Crystals Bracelet", show_retail: true, show_wholesale: true, price_retail: 100, price_wholesale: 50, ypos: 18
 
-cc_bracelet.infos.create! name: "Tips", content: "Tips Content"
-cc_bracelet.infos.create! name: "Sizing", content: "Sizing Content"
-cc_bracelet.infos.create! name: "Gifting", content: "Gifting Content"
-cc_bracelet.infos.create! name: "Uniqueness", content: "Uniqueness Content"
+cc_bracelet.infos.create! name: "Tips", content: "C&C Tips Content"
+cc_bracelet.infos.create! name: "Sizing", content: "C&C Sizing Content"
+cc_bracelet.infos.create! name: "Gifting", content: "C&C Gifting Content"
+cc_bracelet.infos.create! name: "Uniqueness", content: "C&C Uniqueness Content"
 
 cc_bracelet_gem = cc_bracelet.variations.create! name: "Gem", level: 0, has_image: true
 cc_bracelet_gem.variants.create! name: "Howlite", show_retail: true, show_wholesale: true, description: "A descriptive bit of captioning goes here, and it is Howlite indeed. Perhaps another sentence will give us some breadth and depth of understanding. Perhaps not."
@@ -52,10 +52,10 @@ cc_bracelet_size.variants.create! name: "Normal", show_retail: true, show_wholes
 # Wrap Cuff #####################################
 wrap_cuff = bracelets.products.create! name: "Wrap Cuff", show_retail: true, show_wholesale: true, price_retail: 100, price_wholesale: 50, ypos: 18
 
-wrap_cuff.infos.create! name: "Tips", content: "Tips Content"
-wrap_cuff.infos.create! name: "Sizing", content: "Sizing Content"
-wrap_cuff.infos.create! name: "Gifting", content: "Gifting Content"
-wrap_cuff.infos.create! name: "Uniqueness", content: "Uniqueness Content"
+wrap_cuff.infos.create! name: "Tips", content: "Wrap Cuff Tips Content"
+wrap_cuff.infos.create! name: "Sizing", content: "Wrap Cuff Sizing Content"
+wrap_cuff.infos.create! name: "Gifting", content: "Wrap Cuff Gifting Content"
+wrap_cuff.infos.create! name: "Uniqueness", content: "Wrap Cuff Uniqueness Content"
 
 wrap_cuff_color = wrap_cuff.variations.create! name: "Color", level: 0, has_image: true
 wrap_cuff_color.variants.create! name: "Bark", show_retail: true, show_wholesale: true, description: "The one, the only, the classic, the champion. This is our product. You'll love it forever, but you'll also buy them by the fistful."
@@ -74,10 +74,10 @@ wrap_cuff_size.variants.create! name: "Normal", show_retail: true, show_wholesal
 # Fuzzy Cuff ####################################
 fuzzy_cuff = bracelets.products.create! name: "Fuzzy Cuff", show_retail: true, show_wholesale: true, price_retail: 100, price_wholesale: 50, ypos: 33
 
-fuzzy_cuff.infos.create! name: "Tips", content: "Tips Content"
-fuzzy_cuff.infos.create! name: "Sizing", content: "Sizing Content"
-fuzzy_cuff.infos.create! name: "Gifting", content: "Gifting Content"
-fuzzy_cuff.infos.create! name: "Uniqueness", content: "Uniqueness Content"
+fuzzy_cuff.infos.create! name: "Tips", content: "Fuzzy Cuff Tips Content"
+fuzzy_cuff.infos.create! name: "Sizing", content: "Fuzzy Cuff Sizing Content"
+fuzzy_cuff.infos.create! name: "Gifting", content: "Fuzzy Cuff Gifting Content"
+fuzzy_cuff.infos.create! name: "Uniqueness", content: "Fuzzy Cuff Uniqueness Content"
 
 fuzzy_cuff_name = fuzzy_cuff.variations.create! name: "Name", level: 0, has_image: true
 fuzzy_cuff_name.variants.create! name: "Painter", show_retail: true, show_wholesale: true, description: "These cuffs are cute and fuzzy and fluffy and snuggable and totally just lovely wonderful you want them you want them please!"
