d709c5f589695d7309135b4735bca062ca441713
Ivan Reese <ivanreese@gmail.com>
2015-09-17 19:39:08 -0600

    Improve our migration

--- db/migrate/001_create_products.rb
+++ db/migrate/001_create_products.rb
@@ -1,33 +1,23 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :kingdoms do |t| # eg: belt, bracelet
-      # has many :products
-      t.text :name,	              null: false
-      t.index :name,              unique: true
+      t.text :name,	               null: false
+      t.index :name,               unique: true
     end
     
     create_table :products do |t| # eg: wrap cuff, belt bag, nude raw ring
-      # has many :infos
-      # has many :stocks
-      # has many :variations
-      t.belongs_to :kingdom,      required: true, index: true
-      t.text :name,               null: false
-      t.integer :cents_retail,    null: false, default: 0
-      t.integer :cents_wholesale, null: false, default: 0
-      t.boolean :show_retail,     null: false, default: false
-      t.boolean :show_wholesale,  null: false, default: false
-      t.integer :ypos,            null: false, default: 0
-      t.index :name,              unique: true
-    end
-    
-    create_table :infos do |t| # eg: sizing, gifting, uniqueness, tips
-      t.belongs_to :product,      required: true, index: true
-      t.text :content,            null: false
-      t.text :name,               null: false
+      t.belongs_to :kingdom,       required: true, index: true
+      t.text :name,                null: false
+      t.integer :cents_retail,     null: false, default: 0
+      t.integer :cents_wholesale,  null: false, default: 0
+      t.boolean :show_retail,      null: false, default: false
+      t.boolean :show_wholesale,   null: false, default: false
+      t.integer :totem_order,      null: false, default: 0
+      t.integer :ypos,             null: false, default: 0
+      t.index :name,               unique: true
     end
     
     create_table :variations do |t| # eg: color, hardware, size, leaf pattern
-      # has many :variants
       t.belongs_to :product,      required: true, index: true
       t.boolean :has_image,       null: false, default: false
       t.integer :level,           null: false, default: 0
@@ -35,7 +25,6 @@ class CreateProducts < ActiveRecord::Migration
     end
     
     create_table :variants do |t| # eg: red, brass, small, stripes
-      # has many :variant_stock_joins
       t.belongs_to :variation,    required: true, index: true
       t.boolean :default,         null: false, default: false
       t.text :description,        default: nil
@@ -47,15 +36,54 @@ class CreateProducts < ActiveRecord::Migration
       t.integer :sort_order,      null: false, default: 0
     end
     
-    create_table :variant_stock_joins do |t|
-      t.belongs_to :variant,      required: true
-      t.belongs_to :stock,        required: true
+    create_table :infos do |t| # eg: sizing, gifting, uniqueness, tips
+      t.belongs_to :product,      required: true, index: true
+      t.text :name,               null: false
+      t.text :content,            null: false
     end
     
-    create_table :stocks do |t| # how many units are in stock
-      # has many :variant_stock_joins
-      t.belongs_to :product,      required: true
-      t.integer :quantity,        null: false, default: 0
+    create_table :configurations do |t|
+      t.belongs_to :product,      required: true, index: true
+      t.integer :stock,           null: false, default: 0
+    end
+    
+    create_table :configuration_variants do |t|
+      t.belongs_to :configuration, required: true, index: true
+      t.references :variant,       required: true, index: true
+    end
+    
+    create_table :customer do |t|
+      t.text :email,	             null: false
+      t.text :phone_number,        null: false
+      t.text :name,                null: false
+      t.timestamps
+      t.index :email,              unique: true
+    end
+    
+    create_table :merchants do |t|
+      t.references :customer,      required: true, index: true
+      t.text :store_name,          null: false
+    end
+    
+    create_table :orders do |t|
+      t.belongs_to :customer,      required: true, index: true
+      t.integer :status,           null:false, default: 0
+      t.timestamps
+    end
+    
+    create_table :items do |t|
+      t.belongs_to :order,         required: true, index: true
+      
+      # This is useful while the order is being created.
+      # After the order has been submitted, we probably won't need it.
+      t.references :configuration, required: true, index: true
+      
+      # These will remain correct, even if the configuration is changed/deleted
+      t.text :name,                null: false
+      t.integer :cents,            null: false, default: 0
+      
+      t.integer :quantity,         null: false, default: 0
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
-  create_table "current_order_stock_joins", force: :cascade do |t|
-    t.integer "current_order_id"
-    t.integer "stock_id"
+  create_table "configuration_variants", force: :cascade do |t|
+    t.integer "configuration_id"
+    t.integer "variant_id"
   end
 
-  create_table "current_orders", force: :cascade do |t|
-    t.integer  "merchant_id"
+  add_index "configuration_variants", ["configuration_id"], name: "index_configuration_variants_on_configuration_id"
+  add_index "configuration_variants", ["variant_id"], name: "index_configuration_variants_on_variant_id"
+
+  create_table "configurations", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "stock",      default: 0, null: false
+  end
+
+  add_index "configurations", ["product_id"], name: "index_configurations_on_product_id"
+
+  create_table "customer", force: :cascade do |t|
+    t.text     "email",        null: false
+    t.text     "phone_number", null: false
+    t.text     "name",         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "current_orders", ["merchant_id"], name: "index_current_orders_on_merchant_id"
+  add_index "customer", ["email"], name: "index_customer_on_email", unique: true
 
   create_table "infos", force: :cascade do |t|
     t.integer "product_id"
-    t.text    "content",    null: false
     t.text    "name",       null: false
+    t.text    "content",    null: false
   end
 
   add_index "infos", ["product_id"], name: "index_infos_on_product_id"
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "configuration_id"
+    t.text     "name",                         null: false
+    t.integer  "cents",            default: 0, null: false
+    t.integer  "quantity",         default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["configuration_id"], name: "index_items_on_configuration_id"
+  add_index "items", ["order_id"], name: "index_items_on_order_id"
 
   create_table "kingdoms", force: :cascade do |t|
     t.text "name", null: false
@@ -41,15 +66,20 @@ ActiveRecord::Schema.define(version: 2) do
   add_index "kingdoms", ["name"], name: "index_kingdoms_on_name", unique: true
 
   create_table "merchants", force: :cascade do |t|
-    t.text     "phone_number", null: false
-    t.text     "email",        null: false
-    t.text     "your_name",    null: false
-    t.text     "store_name",   null: false
+    t.integer "customer_id"
+    t.text    "store_name",  null: false
+  end
+
+  add_index "merchants", ["customer_id"], name: "index_merchants_on_customer_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "customer_id"
+    t.integer  "status",      default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "merchants", ["email"], name: "index_merchants_on_email", unique: true
+  add_index "orders", ["customer_id"], name: "index_orders_on_customer_id"
 
   create_table "products", force: :cascade do |t|
     t.integer "kingdom_id"
@@ -58,21 +88,12 @@ ActiveRecord::Schema.define(version: 2) do
     t.integer "cents_wholesale", default: 0,     null: false
     t.boolean "show_retail",     default: false, null: false
     t.boolean "show_wholesale",  default: false, null: false
+    t.integer "totem_order",     default: 0,     null: false
     t.integer "ypos",            default: 0,     null: false
   end
 
   add_index "products", ["kingdom_id"], name: "index_products_on_kingdom_id"
   add_index "products", ["name"], name: "index_products_on_name", unique: true
-
-  create_table "stocks", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "quantity",   default: 0, null: false
-  end
-
-  create_table "variant_stock_joins", force: :cascade do |t|
-    t.integer "variant_id"
-    t.integer "stock_id"
-  end
 
   create_table "variants", force: :cascade do |t|
     t.integer "variation_id"
@@ -96,22 +117,5 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   add_index "variations", ["product_id"], name: "index_variations_on_product_id"
-
-  create_table "wholesale_order_items", force: :cascade do |t|
-    t.integer "wholesale_order_id"
-    t.text    "name",                           null: false
-    t.integer "quantity",           default: 0, null: false
-    t.integer "cents",              default: 0, null: false
-  end
-
-  add_index "wholesale_order_items", ["wholesale_order_id"], name: "index_wholesale_order_items_on_wholesale_order_id"
-
-  create_table "wholesale_orders", force: :cascade do |t|
-    t.integer  "merchant_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "wholesale_orders", ["merchant_id"], name: "index_wholesale_orders_on_merchant_id"
 
 end
