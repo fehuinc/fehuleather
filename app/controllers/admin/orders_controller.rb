f3c36a77c1b546a48a21a3f2c547599b7d2fc329
Ivan Reese <ivanreese@gmail.com>
2016-03-19 21:20:42 -0600

    Start working on order display in the admin section

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,5 @@
 class Admin::OrdersController < ApplicationController
+  def index
+    @orders = (RetailOrder.all + WholesaleOrder.all).sort_by(&:created_at)
+  end
 end
--- app/models/wholesale_order.rb
+++ app/models/wholesale_order.rb
@@ -2,30 +2,34 @@ class WholesaleOrder < ActiveRecord::Base
   belongs_to :merchant
   has_many :items, as: :order, class_name: OrderItem, dependent: :destroy
   
-  def item_for_build(build)
-    items.where(build_id: build.id).first
+  def address
+    merchant.address
   end
   
-  def create_item_for_build(build, isWholesale)
-    item = items.new build_id: build.id
-    item.build = build
-    item.build_name = build.build_name
-    item.product_name = build.product.name
-    item.cents = isWholesale ? build.price_wholesale : build.price_retail
-    item.save!
-    item
-  end
-  
-  def update_item_from_build(build, quantity, isWholesale)
-    if quantity > 0
-      item = item_for_build(build) || create_item_for_build(build, isWholesale)
-      item.quantity = quantity
-      item.save! # Will error if this fails, which is what we want
-      item.quantity
-    else
-      item = item_for_build(build)
-      item.destroy! if item.present?
-      0 # We now have 0 quantity
-    end
-  end
+  # def item_for_build(build)
+  #   items.where(build_id: build.id).first
+  # end
+  #
+  # def create_item_for_build(build, isWholesale)
+  #   item = items.new build_id: build.id
+  #   item.build = build
+  #   item.build_name = build.build_name
+  #   item.product_name = build.product.name
+  #   item.cents = isWholesale ? build.price_wholesale : build.price_retail
+  #   item.save!
+  #   item
+  # end
+  #
+  # def update_item_from_build(build, quantity, isWholesale)
+  #   if quantity > 0
+  #     item = item_for_build(build) || create_item_for_build(build, isWholesale)
+  #     item.quantity = quantity
+  #     item.save! # Will error if this fails, which is what we want
+  #     item.quantity
+  #   else
+  #     item = item_for_build(build)
+  #     item.destroy! if item.present?
+  #     0 # We now have 0 quantity
+  #   end
+  # end
 end
