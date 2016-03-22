0cbd427ea6589cf19f48cc48503d66fefb694f93
Ivan Reese <ivanreese@gmail.com>
2016-03-21 20:37:52 -0600

    Rough outline of the orders page, closes #11

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = (RetailOrder.all + WholesaleOrder.all).sort_by(&:created_at)
+    @orders_by_date = (RetailOrder.includes(:items) + WholesaleOrder.includes(:items)).sort_by(&:created_at).reverse.group_by do |order|
+      order.created_at.to_time.strftime("%B %d, %Y")
+    end
   end
 end
