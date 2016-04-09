6526679286fe0d26cd97c34e6fcad6c2c671cbe7
Ivan Reese <ivanreese@gmail.com>
2016-04-09 15:45:24 -0600

    Fix a bad import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-fileupload/basic
-//= require angular/libs/angular
+//= require libs/angular
 //= require libs/take-and-make
 //= require_tree .
