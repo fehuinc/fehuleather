9a251ecbdaba5c863f8b6deeec361df002040f66
Ivan Reese <ivanreese@gmail.com>
2016-05-29 19:30:11 -0600

    Move feature flags to env

--- app/services/feature_flags.rb
+++ app/services/feature_flags.rb
@@ -1,9 +1,6 @@
 class FeatureFlags
   def self.check key
-    case key
-    when :retail then false
-    when :wholesale then false
-    else false
-    end
+    name = "FEATURE_#{key.to_s.upcase}"
+    return ENV[name] || false
   end
 end
