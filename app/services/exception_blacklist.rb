691434480cd60afd4af3f98b767bb89e68f18a89
Ivan Reese <ivanreese@gmail.com>
2016-09-25 19:12:25 -0600

    Update blacklist

--- app/services/exception_blacklist.rb
+++ app/services/exception_blacklist.rb
@@ -1,19 +1,18 @@
 class ExceptionBlacklist
   def self.path_on_404_blacklist(path)
-    # Temporarily nonexistent
-    return true if path.start_with? "/blog"
-
     # Noisy subsystems
     return true if path.start_with? "/assets/application-"
     
     # Nonsense requests
     return true if path.start_with? "/.well-known"
-    return true if path.start_with? "/favicon.ico"
-    return true if path.start_with? "/apple-touch-icon"
-    return true if path.start_with? "/magazine/js"
     return true if path.start_with? "/admin"
     return true if path.start_with? "/apple-app-site-association"
+    return true if path.start_with? "/apple-touch-icon"
+    return true if path.start_with? "/blog"
+    return true if path.start_with? "/browserconfig.xml"
     return true if path.start_with? "/downloader"
+    return true if path.start_with? "/favicon.ico"
+    return true if path.start_with? "/magazine/js"
     return true if path.start_with? "/sitemap"
     return true if path.start_with? "/wordpress"
     return true if path.start_with? "/wp-"
